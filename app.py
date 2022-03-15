c9d250d96bbaea4e3fea5b43a42257c0d17b60fe
bae-code <locing45@gmail.com>
2022-03-15 15:18:04 +0900

    set Api-server

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from flask_restx import Api, Resource
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
+api = Api(app)  # Flask 객체에 Api 객체 등록
 
 
-if __name__ == '__main__':
-    app.run()
+@api.route('/main')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
+class HelloWorld(Resource):
+    def get(self):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
+        return {"hello": "world!"}
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=80)
