14b52ad481b174cbfa3725b62c9fe1ec94072a91
seonghyeon <locing45@gmail.com>
2022-03-15 23:37:45 +0900

    product data save csv

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션
 api = Api(app)  # Flask 객체에 Api 객체 등록
 
 
-@api.route('/main')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
+@api.route('/main/<user>&<workout>')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
 class HelloWorld(Resource):
-    def get(self):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
-        return {"hello": "world!"}
+    def get(self,user,workout):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
+        hi = f"Hi,{user} enjoy your {workout}"
+        return {"hello": hi}
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
